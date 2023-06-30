7b8601e57d85e5a116e1345ce097f5226c5da313
Eligio Biasi <ex_98@live.it>
2023-06-30 15:38:17 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+    <body>
+        <form action="" method="get">
+            <input type="text" name="firstWord">
+            <input type="text" name="wordToCensor">
+            
+            <button type="submit">
+                Premi per censurare
+            </button>
+        </form>
+
+
+    </body>
+</html>
