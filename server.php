f85d5cecb740780fdee7a66891b5b5f31791b40c
Eligio Biasi <ex_98@live.it>
2023-06-30 16:55:06 +0200

    wonderful wods added

--- server.php
+++ server.php
@@ -1,6 +1,8 @@
 <?php
-    $word = $_GET('firstWord');
-    $second_word = $_GET('wordToCensor');
+    $word = $_GET['firstWord'];
+    $second_word = $_GET['wordToCensor'];
+    $banned_words = array("cattivo","cretino fosforescente","infame");
+    $good_words = array("tesoro","cuore","bello")
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +13,15 @@
     <title>server</title>
 </head>
     <body>
-        <?php echo $word ?>
+        <p>
+            <?php echo $word ?>
+        </p>
+        <p>
+            parola da censurare: <?php echo $second_word ?>
+        </p>
+
+        <p>
+            parola nuova e amorevole: <?php echo $word." ".str_ireplace($banned_words,$good_words,$second_word) ?>
+        </p>
     </body>
 </html>
